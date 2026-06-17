74b29f3032190b3d33de3eb32d40457ab27aed4a
Ashton Abdiukov <ashton@abdiukov.com>
2026-06-17 15:57:35 -0400

    feat: remove RestSharp dependency (#31)

--- tests/Captcha.FunctionalTests/Support/TestBase.cs
+++ tests/Captcha.FunctionalTests/Support/TestBase.cs
@@ -2,17 +2,16 @@ namespace Captcha.FunctionalTests.Support;
 
 using Microsoft.AspNetCore.Mvc.Testing;
 using Reqnroll;
-using RestSharp;
 public class TestBase
 {
-    protected RestClient Client { get; set; }
+    protected HttpClient Client { get; set; }
     protected ScenarioContext Context { get; set; }
 
     protected TestBase(ScenarioContext context)
     {
         var webApplicationFactory = new WebApplicationFactory<Program>();
 
-        Client = new RestClient(webApplicationFactory.CreateClient());
+        Client = webApplicationFactory.CreateClient();
         Context = context;
     }
 }
